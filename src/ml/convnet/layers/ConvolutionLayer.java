fbada8330ce87f174baa5598ef6b0172f7e3862a
Sek Cheong <scheong@gmail.com>
2017-03-18 19:26:11 -0500

    more items

--- src/ml/convnet/layers/ConvolutionLayer.java
+++ src/ml/convnet/layers/ConvolutionLayer.java
@@ -1,33 +1,49 @@
 package ml.convnet.layers;
 
-public class ConvolutionLayer extends Layer {	
-	private int _width;  //filter width
-	private int _height; //filter height
-	private int _depth;  //filter depth
+public class ConvolutionLayer extends Layer {
+	private int _width; // filter width
+	private int _height; // filter height
+	private int _depth; // filter depth
 	private int _stride;
 	private int _padding;
 	private Layer _activation;
-	
-	//sx:3, sy: 4, pad:1, filters:10, stride:1, activation:'relu'
+
+
+	public ConvolutionLayer(int width, int filters, int stride, int padding, Layer activation) {
+		initLayer(width, 0, filters, stride, padding, activation);
+	}
+
+
 	public ConvolutionLayer(int width, int height, int filters, int stride, int padding, Layer activation) {
+		initLayer(width, height, filters, stride, padding, activation);
+	}
+
+
+	private void initLayer(int width, int height, int filters, int stride, int padding, Layer activation) {
 		_width = width;
-		_height = height;
+		_height = height>0 ?  height : width;
 		_depth = filters;
 		_stride = stride;
 		_padding = padding;
 		_activation = activation;
+
+//		_outWidth = Math.floor((this.inputWidth() + padding * 2 - width) / stride + 1);
+//		this.out_sy = Math.floor((this.in_sy + this.pad * 2 - this.sy) / this.stride + 1);
+		this.type(LayerType.convolution);
 	}
+
 
 	@Override
 	void forward() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 
 	@Override
 	void backward() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
